3e5c4d631eadf66a775d355cc8c7cdac8e3f90a2
celine <wisohyun0@gmail.com>
2025-11-07 02:06:49 +0900

    feat: #6 ui export

---
+++ src/components/index.ts
@@ -0,0 +1,3 @@
+export { default as Button } from "@/components/ui/button/button";
+export { default as FloatingWrapper } from "@/components/ui/floating/floating-wrapper";
+export { default as FloatingButton } from "@/components/ui/floating/floating-button";
