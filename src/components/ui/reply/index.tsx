a53c139b221c6ac11c391f1db9ee1fcc2ed33a4b
Yeonsu Lee <170384198+suuuuya@users.noreply.github.com>
2025-11-08 05:02:53 +0900

    feat: #23 작성글 수정 상태 관리 훅 구현 및 적용

--- src/components/ui/reply/index.tsx
+++ src/components/ui/reply/index.tsx
@@ -1,23 +1,55 @@
+"use client";
+
 //import cn from "@/lib/cn";
 import { Comments } from "@/types/comments";
-import { Avatar } from "@/components/ui";
+import { Avatar, Button } from "@/components/ui";
 import { getTimeAgo } from "@/lib/utils";
+import useEditable from "@/hooks/use-editable";
 
 type ReplyProps = {
   comment: Comments;
 };
 
 export default function Reply({ comment }: ReplyProps) {
-  const UserImageUrl = comment.user.image;
+  const {
+    isEditing,
+    editedContent,
+    startEditing,
+    cancelEditing,
+    saveEditing,
+    isSaveDisabled,
+    setEditedContent,
+  } = useEditable(comment.content);
 
   return (
     <div>
-      <p>{comment.content}</p>
-      <div>
-        <Avatar image={UserImageUrl} />
-        <span>{comment.user.nickname}</span>
-      </div>
-      <span>{getTimeAgo(comment.createdAt)}</span>
+      {isEditing ? (
+        <>
+          <textarea
+            id={`comment-${comment.id}`}
+            value={editedContent}
+            placeholder="수정할 댓글을 입력해주세요"
+            onChange={e => setEditedContent(e.target.value)}
+          />
+          <div>
+            <Button onClick={cancelEditing}>취소</Button>
+            <Button onClick={saveEditing} disabled={isSaveDisabled}>
+              저장
+            </Button>
+          </div>
+        </>
+      ) : (
+        <>
+          <Button onClick={startEditing}>수정</Button>
+
+          <p>{comment.content}</p>
+          <div>
+            <Avatar image={comment.user.image} />
+            <span>{comment.user.nickname}</span>
+          </div>
+          <span>{getTimeAgo(comment.createdAt)}</span>
+        </>
+      )}
     </div>
   );
 }
