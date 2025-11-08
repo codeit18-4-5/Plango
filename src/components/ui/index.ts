cb516b26b27bd321a7f54b72b1591c94c087ee26
celine <wisohyun0@gmail.com>
2025-11-09 00:38:30 +0900

    feat: #16 input,checkbox export

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,3 +1,5 @@
 export { default as Button } from "@/components/ui/button/button";
 export { default as CircleButton } from "@/components/ui/button/circle-button";
 export { default as FloatingWrapper } from "@/components/ui/floating/floating-wrapper";
+export { default as Input } from "@/components/ui/input/input";
+export { default as Checkbox } from "@/components/ui/input/checkbox";
