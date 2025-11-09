682e5dda003dab409961f11f257f861890e16b84
Yeonsu Lee <170384198+suuuuya@users.noreply.github.com>
2025-11-10 03:16:52 +0900

    style: #34 ImgUpload 스타일 적용 임시저장

--- src/components/ui/img-upload/img-upload.tsx
+++ src/components/ui/img-upload/img-upload.tsx
@@ -2,8 +2,10 @@
 
 import { useRef } from "react";
 import { useImageUpload, useFileDrop } from "@/hooks";
+import { IMG_UPLOAD_STYLES } from "./img-upload.styles";
 import Image from "next/image";
 import IcCancel from "@/assets/icons/ic-cancel.svg";
+import IcPlus from "@/assets/icons/ic-plus.svg";
 
 export default function ImgUpload() {
   const { preview, error, handleFile, removeImage } = useImageUpload();
@@ -23,26 +25,41 @@ export default function ImgUpload() {
   };
 
   return (
-    <div>
-      <div onDragOver={onDragOver} onDragLeave={onDragLeave} onDrop={onDrop}>
+    <>
+      <div
+        className={IMG_UPLOAD_STYLES.wrapper}
+        onDragOver={onDragOver}
+        onDragLeave={onDragLeave}
+        onDrop={onDrop}
+      >
         {!preview ? (
-          <input
-            ref={inputRef}
-            type="file"
-            accept="image/*"
-            onChange={onChange}
-            aria-label="이미지 업로드"
-          />
+          <>
+            <input ref={inputRef} type="file" accept="image/*" onChange={onChange} />
+            <div>
+              <IcPlus className={IMG_UPLOAD_STYLES.icon.plus} />
+              <span>이미지 등록</span>
+            </div>
+          </>
         ) : (
           <>
-            <Image src={preview.image} alt={preview.name} fill />
-            <button type="button" onClick={removeImage} aria-label="업로드한 이미지 삭제">
-              <IcCancel />
+            <Image
+              src={preview.image}
+              alt={preview.name}
+              fill
+              className={IMG_UPLOAD_STYLES.image}
+            />
+            <button
+              type="button"
+              onClick={removeImage}
+              aria-label="등록 이미지 삭제"
+              className={IMG_UPLOAD_STYLES.button}
+            >
+              <IcCancel className={IMG_UPLOAD_STYLES.icon.cancel} />
             </button>
           </>
         )}
       </div>
-      {error && <span>{error}</span>}
-    </div>
+      {error && <span className={IMG_UPLOAD_STYLES.errorMsg}>{error}</span>}
+    </>
   );
 }
