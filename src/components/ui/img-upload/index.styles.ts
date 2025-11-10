cbe074900995ae97502bfaa0625cd7dbd8fcced0
Yeonsu Lee <170384198+suuuuya@users.noreply.github.com>
2025-11-10 18:49:00 +0900

    style: #34 svgr 아이콘 적용 완료

--- src/components/ui/img-upload/index.styles.ts
+++ src/components/ui/img-upload/index.styles.ts
@@ -10,15 +10,15 @@ export const IMG_UPLOAD_STYLES = {
     ),
   input: cn("absolute w-0 h-0 overflow-hidden opacity-0 pointer-events-none"),
   label: cn(
-    "absolute w-full h-full top-0 left-0 cursor-pointer text-gray-400 grid gap-[12px] justify-center items-center text-body-m content-center",
+    "absolute w-full h-full top-0 left-0 cursor-pointer [&>span]:text-gray-400 grid gap-[12px] justify-center items-center text-body-m content-center",
   ),
   image: cn("object-contain"),
   button: cn(
     "absolute top-0 left-0 w-full h-full flex justify-center items-center bg-dimmed-overlay z-3",
   ),
   icon: {
-    plus: cn("w-[24px] h-[24px] text-gray-400", "tablet:w-[48px] tablet:h-[48px]"),
-    cancel: cn("w-[36px] h-[36px]", "tablet:w-[40px] tablet:h-[40px]"),
+    plus: cn("m-auto w-[24px] h-[24px] stroke-gray-400", "tablet:w-[48px] tablet:h-[48px]"),
+    cancel: cn("m-auto w-[36px] h-[36px] stroke-gray-100", "tablet:w-[40px] tablet:h-[40px]"),
   },
   errorMsg: cn("text-red-400 text-caption mt-2 block"),
 };
