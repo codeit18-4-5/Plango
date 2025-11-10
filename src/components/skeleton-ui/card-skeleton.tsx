d12aa9de3b70cfb8723526917aac51e95b822f2a
Yeonsu Lee <170384198+suuuuya@users.noreply.github.com>
2025-11-11 00:38:41 +0900

    design: #35 react-loading-skeleton 라이브러리 설치 및 스토리북 skeleton UI 문서화

---
+++ src/components/skeleton-ui/card-skeleton.tsx
@@ -0,0 +1,6 @@
+//import Skeleton from "react-loading-skeleton";
+//import "react-loading-skeleton/dist/skeleton.css";
+
+export default function CardSkeleton() {
+  return <div>카드 스켈레톤 컴포넌트</div>;
+}
