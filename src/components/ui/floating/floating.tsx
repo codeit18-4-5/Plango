d98cfb67dc2059a9618f25896ea06a0a2e48b0db
celine <wisohyun0@gmail.com>
2025-11-10 15:50:00 +0900

    fix: #15 className 추가

--- src/components/ui/floating/floating.tsx
+++ src/components/ui/floating/floating.tsx
@@ -6,12 +6,13 @@ type FloatingProps = {
   children?: ReactNode;
 };
 
-export default function Floating({ children }: FloatingProps) {
+export default function Floating({ className, children }: FloatingProps) {
   return (
     <div
       className={cn(
         "floating-wrapper flex flex-col items-end gap-3",
         "fixed bottom-6 right-6 tablet:bottom-10 tablet:right-10",
+        className,
       )}
     >
       {children}
