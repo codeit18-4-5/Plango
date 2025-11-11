8903d283148995349892dadfa1cc58fee55e9bdc
celine <wisohyun0@gmail.com>
2025-11-12 01:16:53 +0900

    feat: #51 input 컴포넌트 타입변경

--- src/components/ui/input/input-password.tsx
+++ src/components/ui/input/input-password.tsx
@@ -1,6 +1,6 @@
 "use client";
 import cn from "@/lib/cn";
-import { useContext } from "react";
+import { useContext, type ComponentPropsWithoutRef } from "react";
 import inputStyle from "./input.style";
 import { InputContext } from "./input.context";
 import Button from "../button/button";
@@ -8,14 +8,9 @@ import IcVisible from "@/assets/icons/ic-visible.svg";
 import IcVisibleOff from "@/assets/icons/ic-visible-false.svg";
 import useToggle from "@/hooks/use-toggle";
 
-type InputPasswordProps = {
-  name?: string;
-  disabled?: boolean;
-  className?: string;
-};
+type InputPasswordProps = Omit<ComponentPropsWithoutRef<"input">, "type">;
 
 export default function InputPassword({
-  name,
   disabled = false,
   className,
   ...props
@@ -28,7 +23,6 @@ export default function InputPassword({
       <input
         type={isOpen ? "text" : "password"}
         id={ctx?.id}
-        name={name}
         disabled={disabled}
         className={cn(
           inputStyle({ intent: "password", disabled, error: Boolean(ctx?.errorMsg) }),
