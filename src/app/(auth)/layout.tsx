8d0b5da9c55be7672f7117fac4e509b79bfea1b4
celine <wisohyun0@gmail.com>
2025-11-12 14:26:49 +0900

    design: #51 반응형 디자인

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -2,7 +2,7 @@ import { Container } from "@/components/layout";
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
-    <Container as="main" className="flex min-h-dvh max-w-[460px] flex-col justify-center">
+    <Container as="main" className="flex min-h-dvh max-w-[460px] flex-col tablet:justify-center">
       <div>{children}</div>
     </Container>
   );
