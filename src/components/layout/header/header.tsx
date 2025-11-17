c8d0b6b5a845e8e8646e47c304a463c55253d717
sejin5 <sejin5.dev@gmail.com>
2025-11-17 15:42:15 +0900

    fix: #41 미사용 변수 삭제

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -14,24 +14,10 @@ import { DropdownOption } from "@/types/option";
 interface HeaderProps {
   isLoginPage: boolean;
   isUser?: boolean;
-  isGroup?: boolean;
   groups?: DropdownOption[];
 }
 
-const groups: DropdownOption[] = [
-  {
-    id: 1,
-    name: "test1",
-    image: "",
-  },
-  {
-    id: 2,
-    name: "test2",
-    image: "",
-  },
-];
-
-export default function Header({ isLoginPage, isUser, isGroup }: HeaderProps) {
+export default function Header({ isLoginPage, isUser, groups }: HeaderProps) {
   const [open, setOpen] = useState(false);
 
   const handleMenuClick = (newOpen: boolean) => () => {
@@ -51,7 +37,7 @@ export default function Header({ isLoginPage, isUser, isGroup }: HeaderProps) {
 
   return (
     <div className="relative">
-      {open && <HeaderSidebar groups={groups} onClick={handleMenuClick(false)} />}
+      {open && groups && <HeaderSidebar groups={groups} onClick={handleMenuClick(false)} />}
       <div className="mx-auto flex w-full max-w-[1248px] items-center justify-between px-[16px] py-[18px] tablet:px-[24px] tablet:py-[20px]">
         <div className="flex items-center justify-between">
           <IcMenu
@@ -62,7 +48,7 @@ export default function Header({ isLoginPage, isUser, isGroup }: HeaderProps) {
             <Logo className="h-[20px] w-[100px] tablet:mr-[32px] desktop:mr-[40px] desktop:h-[32px] desktop:w-[158px]" />
           </Link>
           <div className="hidden w-[200px] tablet:block tablet:flex tablet:items-center tablet:justify-between">
-            {isGroup && <HeaderDropdown groups={groups} />}
+            {groups && <HeaderDropdown groups={groups} />}
             <Link href="/">자유게시판</Link>
           </div>
         </div>
