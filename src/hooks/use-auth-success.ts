89eb79292269f6be4e507702a3282eece914427f
celine <wisohyun0@gmail.com>
2025-11-18 13:41:24 +0900

    feat: #55 로그인 성공시 토큰 저장 hook

---
+++ src/hooks/use-auth-success.ts
@@ -0,0 +1,52 @@
+import { useAuthStore, useUIStore } from "@/store/auth.store";
+import { User } from "@/types/user";
+import { useRouter } from "next/navigation";
+import useLogout from "./use-logout";
+
+/**
+ * 로그인 / 회원가입 성공 이후 공통 처리 훅
+ * @author sohyun
+ */
+
+type AuthSuccessPayload = {
+  accessToken: string;
+  refreshToken: string;
+  user: User;
+};
+
+const useAuthSuccess = () => {
+  const router = useRouter();
+  const { setAccessToken, setUser } = useAuthStore();
+  const { setAuthError } = useUIStore();
+  const logout = useLogout();
+
+  return async (authData: AuthSuccessPayload) => {
+    const { accessToken, refreshToken, user } = authData;
+
+    try {
+      // refreshToken을 HttpOnly 쿠키로 저장
+      const res = await fetch("/api/auth/set-refresh-token", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ refreshToken }),
+      });
+
+      if (!res.ok) {
+        // 쿠키 저장 실패
+        await logout({ isRedirect: false });
+        setAuthError("로그인 중 오류가 발생했습니다.");
+        return;
+      }
+
+      // accessToken, 유저 정보 저장
+      setAccessToken(accessToken);
+      setUser(user);
+
+      router.replace("/");
+    } catch {
+      await logout({ isRedirect: false });
+      setAuthError("네트워크 오류가 발생했습니다");
+    }
+  };
+};
+export default useAuthSuccess;
