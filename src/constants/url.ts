d55a313efabf853b983d607c88a0395d8b72e540
celine <wisohyun0@gmail.com>
2025-11-18 13:42:25 +0900

    feat: #52 axios 인증 필터 유틸 작성

---
+++ src/constants/url.ts
@@ -0,0 +1,11 @@
+export const NO_AUTH_URLS = [
+  "/public",
+  "/user/send-reset-password-email",
+  "/user/reset-password",
+  "/oauthApps",
+  "/auth/signUp",
+  "/auth/signIn",
+  "/auth/refresh-token",
+  "/auth/signIn/KAKAO",
+];
+export const NO_AUTH_GET: (string | RegExp)[] = ["/articles", /^\/articles\/\d+\/comments$/];
