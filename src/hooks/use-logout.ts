fafcfe4b428cff4e59293cafcc261f5d3cbf5089
celine <wisohyun0@gmail.com>
2025-11-18 13:41:46 +0900

    feat: #55 로그아웃 시 토큰 삭제 hook

---
+++ src/hooks/use-logout.ts
@@ -0,0 +1,22 @@
+import { useAuthStore } from "@/store/auth.store";
+import { useRouter } from "next/navigation";
+/**
+ * 로그아웃 처리 훅, 필요시 메인 페이지 리다이렉트
+ * @author sohyun
+ */
+const useLogout = () => {
+  const router = useRouter();
+  const { clearAuth } = useAuthStore();
+
+  return async ({ isRedirect = true } = {}) => {
+    try {
+      // 쿠키 삭제
+      await fetch("/api/auth/logout", { method: "POST" });
+    } finally {
+      // 스토어 초기화
+      clearAuth();
+    }
+    if (isRedirect) router.replace("/");
+  };
+};
+export default useLogout;
