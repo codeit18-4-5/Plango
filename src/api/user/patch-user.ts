0ebb7033aec7b5f8a717449403f4ad3c651a998a
celine <wisohyun0@gmail.com>
2025-11-20 00:15:52 +0900

    feat: #55 유저 테스트 api 추가

--- src/api/user/patch-user.ts
+++ src/api/user/patch-user.ts
@@ -1,8 +1,8 @@
 import axiosInstance from "@/lib/axios";
 import { UserUpdate } from "@/types/user";
 
-const PatchUser = async (user: UserUpdate) => {
+const patchUser = async (user: UserUpdate) => {
   const res = await axiosInstance.patch("/user", user);
   return res;
 };
-export default PatchUser;
+export default patchUser;
