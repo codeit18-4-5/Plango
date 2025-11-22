95e1a9b123c80471ec7479d1d07f63e301135d7c
luli <nidor022@gmail.com>
2025-11-22 20:57:27 +0900

    Revert "Merge branch 'test2' into feat/tasklist"
    This reverts commit dd41bb3d1e9520435ce7c349ef33e48ed921db9a, reversing
    changes made to e2ad1aa25d38a9454ff03bc8ad4da8d35e23717f.

--- src/app/(routes)/team/[id]/tasklist/@modal/(.)[taskId]/page.tsx
+++ src/app/(routes)/team/[id]/tasklist/@modal/(.)[taskId]/page.tsx
@@ -1,11 +1,12 @@
-import TaskDetailModal from "@/components/features/tasklist/task-detail-modal";
+// import TaskDetailModal from "@/components/features/tasklist/task-detail-modal";
 
 export default async function TaskDetailModalPage({
   params,
 }: {
   params: Promise<{ taskId: string; id: string }>;
 }) {
-  const { taskId, id } = await params;
+  const { taskId } = await params;
 
-  return <TaskDetailModal taskId={Number(taskId)} groupId={Number(id)} />;
+  return <div>나중 페이지 들어감{taskId}</div>;
+  // <TaskDetailModal taskId={Number(taskId)} />;
 }
