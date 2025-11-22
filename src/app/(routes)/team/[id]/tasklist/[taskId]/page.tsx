cfc41f8b2002468928a2638f6a95f2d0f166e14c
luli <nidor022@gmail.com>
2025-11-22 15:43:46 +0900

    refactor: #121 상세보기 페이지 일단 주석처리

--- src/app/(routes)/team/[id]/tasklist/[taskId]/page.tsx
+++ src/app/(routes)/team/[id]/tasklist/[taskId]/page.tsx
@@ -1,4 +1,4 @@
-import TaskDetailModal from "@/components/features/tasklist/task-detail-modal";
+// import TaskDetailModal from "@/components/features/tasklist/task-detail-modal";
 
 export default async function TaskDetailPage({ params }: { params: { taskId: string } }) {
   const { taskId } = await params;
@@ -6,7 +6,8 @@ export default async function TaskDetailPage({ params }: { params: { taskId: str
   // 전체 페이지로 표시 (모달 아님)
   return (
     <div className="min-h-screen bg-gray-900">
-      <TaskDetailModal taskId={Number(taskId)} />
+      <div>나중 페이지 들어감{taskId}</div>
+      {/* <TaskDetailModal taskId={Number(taskId)} /> */}
     </div>
   );
 }
