c5c1fa9ad73f37bed1c614171dca6e62fed78d25
luli <nidor022@gmail.com>
2025-11-22 15:22:53 +0900

    feat: #106 tasklist get용 api

---
+++ src/api/tasklist/index-server.ts
@@ -0,0 +1,31 @@
+import serverAxios from "@/lib/axios-server";
+import { TaskListProps } from "@/types/task";
+
+export async function getGroupTaskListsforServer(groupId: number) {
+  if (!groupId) return [];
+
+  try {
+    const res = await serverAxios.get(`/groups/${groupId}`);
+    return res.data;
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+}
+
+export async function getTaskListForServer({ groupId, taskListId, date }: TaskListProps) {
+  if (!groupId || !taskListId || !date) return [];
+
+  try {
+    const params = new URLSearchParams();
+    params.append("date", date);
+
+    const res = await serverAxios.get(
+      `/groups/${groupId}/task-lists/${taskListId}?${params.toString()}`,
+    );
+    return res.data;
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+}
---
+++ src/api/tasklist/index.ts
@@ -0,0 +1,47 @@
+"use client";
+
+import { TaskDetailProps, TaskListProps } from "@/types/task";
+import axiosInstance from "@/lib/axios";
+
+export async function getGroupTaskLists(groupId: number) {
+  if (!groupId) return [];
+
+  try {
+    const res = await axiosInstance.get(`/groups/${groupId}`);
+    return res.data;
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+}
+
+export async function getTaskList({ groupId, taskListId, date }: TaskListProps) {
+  if (!groupId || !taskListId || !date) return [];
+
+  try {
+    const params = new URLSearchParams();
+    params.append("date", date);
+
+    const res = await axiosInstance.get(
+      `/groups/${groupId}/task-lists/${taskListId}?${params.toString()}`,
+    );
+    return res.data;
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+}
+
+export async function getTaskDetail({ groupId, taskListId, taskId }: TaskDetailProps) {
+  if (!groupId || !taskListId || !taskId) return [];
+
+  try {
+    const res = await axiosInstance.get(
+      `/groups/${groupId}/task-lists/${taskListId}/tasks/${taskId}`,
+    );
+    return res.data;
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+}
