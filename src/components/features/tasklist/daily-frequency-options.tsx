ee5374cf38e9c64640c27fd82f37d3124e62abb5
luli <nidor022@gmail.com>
2025-11-22 15:39:13 +0900

    feat: #121 task 생성 모달 ui, 유효성검사까지 (122, 123)

---
+++ src/components/features/tasklist/daily-frequency-options.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { Button } from "@/components/ui";
+import cn from "@/lib/cn";
+import { DailyFrequency } from "@/types/date-format-type";
+
+interface dailyFrequencyProps {
+  chooseIndexArray?: number[];
+  onChange: (dayIndex: number) => void;
+}
+
+export default function DailyFrequencyOptions({
+  chooseIndexArray = [],
+  onChange,
+}: dailyFrequencyProps) {
+  const dailyList = DailyFrequency;
+
+  return (
+    <div className="flex w-full gap-[4.6px]">
+      {dailyList.map(list => {
+        const isSelected = chooseIndexArray.includes(list.value);
+
+        return (
+          <Button
+            key={list.value}
+            intent="tertiary"
+            className={cn("px-[13.8px]", isSelected ? "bg-pink-400 text-white" : "")}
+            onClick={() => onChange(list.value)}
+          >
+            {list.label}
+          </Button>
+        );
+      })}
+    </div>
+  );
+}
