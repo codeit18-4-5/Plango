4e6794bad55023dfe258103e2862fbfe6c58755f
sejin5 <sejin5.dev@gmail.com>
2025-11-24 16:08:53 +0900

    delete: #49 api 중복 삭제

--- src/api/images/index.ts
+++
@@ -1,8 +0,0 @@
-import axiosInstance from "@/lib/axios";
-
-const postImage = async (param?: string): Promise<string | undefined> => {
-  const res = await axiosInstance.post("/images/upload", param);
-  return res.data;
-};
-
-export default postImage;
