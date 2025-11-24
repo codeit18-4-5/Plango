4a0f096db9fd22c59b1a0b2cef0afdb76d6d562c
celine <wisohyun0@gmail.com>
2025-11-24 15:45:56 +0900

    feat: #55 useSearchParams 사용으로 인해 Suspense 추가

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,43 +1,12 @@
 "use client";
 
-import postSignIn from "@/api/auth/post-signin";
-import {
-  AuthDivider,
-  AuthTitle,
-  AuthLink,
-  SocialAuthButton,
-  SignInFormFields,
-} from "@/components/features/auth";
-import { Form } from "@/components/ui";
-import { useAuthSuccess } from "@/hooks";
-import { loginErrorHandler } from "@/lib/error";
-import { signInSchema, SignInSchema } from "@/lib/schema";
-import { zodResolver } from "@hookform/resolvers/zod";
+import { Suspense } from "react";
+import { Login } from "@/components/features/auth";
 
-const title = "로그인";
-
-export default function Signup() {
-  const authSuccess = useAuthSuccess();
-
-  const handleSubmit = async (data: SignInSchema) => {
-    const res = await postSignIn(data);
-    await authSuccess(res);
-  };
+export default function Page() {
   return (
-    <div>
-      <AuthTitle>{title}</AuthTitle>
-      <Form<SignInSchema>
-        onSubmit={handleSubmit}
-        resolver={zodResolver(signInSchema)}
-        onServerError={loginErrorHandler}
-        mode="onBlur"
-        reValidateMode="onBlur"
-      >
-        <SignInFormFields />
-      </Form>
-      <AuthLink message="아직 계정이 없으신가요?" linkText="가입하기" href="/signup" />
-      <AuthDivider />
-      <SocialAuthButton title={title} />
-    </div>
+    <Suspense fallback={<p>회원가입 준비중...</p>}>
+      <Login />
+    </Suspense>
   );
 }
--- src/components/features/auth/index.ts
+++ src/components/features/auth/index.ts
@@ -7,3 +7,7 @@ export {
   SocialAuthButton,
   AuthErrorModal,
 } from "@/components/features/auth/auth-page";
+
+export { default as Login } from "@/components/features/auth/login";
+export { default as Signup } from "@/components/features/auth/signup";
+export { default as KakaoRedirect } from "@/components/features/auth/kakao-redirect";
