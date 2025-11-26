ccf2583fe0d8d1d556637dafb40bcaf82bd7eefe
sejin5 <sejin5.dev@gmail.com>
2025-11-27 00:57:49 +0900

    style: #149 스타일, props, import 수정

---
+++ src/components/features/team/team.props.ts
@@ -0,0 +1,27 @@
+export const badgeChartProps = {
+  colorCircle: "var(--white)",
+  colorSlice: "var(--pink-400)",
+  stroke: 15,
+  speed: 30,
+  cut: 0,
+  rotation: 90,
+  inverse: true,
+  animationOff: false,
+  fill: "none",
+  round: true,
+  number: false,
+};
+
+export const reportChartProps = {
+  colorCircle: "var(--gray-700)",
+  stroke: 15,
+  speed: 60,
+  cut: 0,
+  rotation: 90,
+  animationOff: false,
+  fill: "none",
+  inverse: true,
+  round: true,
+  number: false,
+  linearGradient: ["var(--pink-300)", "var(--pink-700)"],
+};
