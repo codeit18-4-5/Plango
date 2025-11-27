4648cfeea0806332d9f0d22ff61a213835ecff9f
celine <wisohyun0@gmail.com>
2025-11-28 02:08:45 +0900

    feat: #71 SSR 실패시 처리 및 CSR 실패시 재시도 ui 추가

--- src/app/(routes)/my/history/page.tsx
+++ src/app/(routes)/my/history/page.tsx
@@ -1,29 +1,39 @@
 "use client";
 
-import { Container } from "@/components/layout";
 import { useQuery } from "@tanstack/react-query";
-import HistoryList from "@/components/features/my/history-list";
+import HistoryList, { HistoryEmpty } from "@/components/features/my/history-list";
 import { UserHistoryResponse } from "@/types/user";
 import getHistory from "@/api/user/get-history";
+import { Button } from "@/components/ui";
+import HistoryListSkeleton from "@/components/skeleton-ui/history-list-skeleton";
 
 export default function MyHistory() {
-  const { data: historyItems } = useQuery<UserHistoryResponse>({
+  const {
+    data: historyItems,
+    isLoading,
+    isError,
+    refetch,
+  } = useQuery<UserHistoryResponse>({
     queryKey: ["history"],
     queryFn: getHistory,
     staleTime: 1000 * 60 * 5,
+    retry: 1,
   });
   const historyDone = historyItems?.tasksDone ?? [];
 
-  return (
-    <Container className="full-scroll-h flex flex-col pb-6">
-      <h2 className="text-heading-m font-bold tablet:text-heading-s">마이 히스토리</h2>
-      {historyDone?.length === 0 ? (
-        <div className="flex-1 content-center text-center">
-          <p className="text-sm text-gray-500">아직 히스토리가 없습니다.</p>
-        </div>
-      ) : (
-        <HistoryList tasksDone={historyDone} />
-      )}
-    </Container>
-  );
+  if (isLoading) return <HistoryListSkeleton />;
+
+  if (isError) {
+    return (
+      <HistoryEmpty msg="히스토리를 불러오지 못했습니다.">
+        <Button className="mt-4" size="md" onClick={() => refetch()}>
+          다시 시도
+        </Button>
+      </HistoryEmpty>
+    );
+  }
+
+  if (historyDone?.length === 0) return <HistoryEmpty msg="아직 히스토리가 없습니다." />;
+
+  return <HistoryList tasksDone={historyDone} />;
 }
