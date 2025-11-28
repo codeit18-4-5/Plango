66440413a67c83fa3f8ece01372f3a7b9de710d9
luli <nidor022@gmail.com>
2025-11-28 12:07:34 +0900

    refactor: #121 alert, modal css조금변경 -> 딱히 변한거없음

--- src/components/ui/alert/alert.styles.ts
+++ src/components/ui/alert/alert.styles.ts
@@ -1,7 +1,7 @@
 import { cva } from "class-variance-authority";
 
 export const alertOverlayStyle = [
-  "fixed inset-0 z-[9000] flex items-center justify-center",
+  "fixed inset-0 flex items-center justify-center z-[9000] ",
   "custom-dialog-backdrop bg-transparent overflow-hidden",
   "rounded-t-xl",
 ].join(" ");
@@ -9,7 +9,7 @@ export const alertOverlayStyle = [
 export const alertContainerStyle = [
   "flex flex-col bg-gray-800 px-4 pb-8 pt-4 text-center items-center rounded-t-xl h-auto w-[100vw] max-w-[384px]",
   "p-0 tablet:px-4 tablet:pb-8 tablet:pt-4",
-  "absolute bottom-0 mobile:relative mobile:bottom-auto mobile:rounded-bl-0 mobile:rounded-b-xl",
+  "fixed bottom-0 mobile:relative mobile:bottom-auto mobile:rounded-bl-0 mobile:rounded-b-xl",
 ].join(" ");
 
 export const alertIcon = "mt-[24px] w-[24px] h-[24px]";
