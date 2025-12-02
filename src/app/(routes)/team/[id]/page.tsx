736583c33ed88a07298697cbde5e27627305f699
sejin5 <sejin5.dev@gmail.com>
2025-12-02 14:45:38 +0900

    fix: #198 params 수정

--- src/app/(routes)/team/[id]/page.tsx
+++ src/app/(routes)/team/[id]/page.tsx
@@ -5,7 +5,7 @@ import TeamClientPages from "./team-client";
 
 export const dynamic = "force-dynamic";
 
-export default async function TeamPages({ params }: { params: { id: string } }) {
+export default async function TeamPages({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
 
   const groupId = Number(id);
